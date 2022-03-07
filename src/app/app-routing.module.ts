fdd8b793a826050a4b0575d5260a558a48d09b59
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-03-07 18:21:27 -0300

    adicionado direcionamento de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProjectsComponent } from './views/projects/projects.component';
 import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path:'',
     component: HomeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
   }
 ];
 
