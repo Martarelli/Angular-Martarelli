7308bec30b67b30ed61fb23a871d16f8c0d420a6
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-04-19 19:58:32 -0300

    add query via TAP

--- src/app/shared/services/api-nasa-exoplanets.service.ts
+++ src/app/shared/services/api-nasa-exoplanets.service.ts
@@ -7,7 +7,9 @@ import { Injectable } from '@angular/core';
 })
 export class ApiNasaExoplanetsService {
 
-  queryAPI:string = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&where=koi_disposition%20like%20%27CONFIRMED%27%20&format=json';
+  queryAPI:string = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,hostname,discoverymethod,disc_year,pl_orbper,pl_masse+from+ps&format=json';
+
+  // queryAPI:string = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&where=koi_disposition%20like%20%27CONFIRMED%27%20&format=json';
 
   constructor(private httpClient: HttpClient) { }
 
