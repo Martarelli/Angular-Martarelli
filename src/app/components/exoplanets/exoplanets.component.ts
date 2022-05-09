3ee69934a21039d536da31914432e14d9171ceb6
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-05-09 16:28:15 -0300

    implementando paginator

--- src/app/components/exoplanets/exoplanets.component.ts
+++ src/app/components/exoplanets/exoplanets.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { ApiNasaExoplanetsService } from './../../shared/services/api-nasa-exoplanets.service';
@@ -8,12 +9,17 @@ import { ApiNasaExoplanetsService } from './../../shared/services/api-nasa-exopl
   templateUrl: './exoplanets.component.html',
   styleUrls: ['./exoplanets.component.css']
 })
-export class ExoplanetsComponent implements OnInit {
+
+
+export class ExoplanetsComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator | undefined;
 
   exoplanets: Array<any> = [];
-  lengthPaginator: string = '10';
-  pageSizePaginator:string = '10';
-  pageSizeOptionsPaginator: number[] = [15, 30, 50, 100];
+  length: string = '';
+  pageSize:string = '';
+  pageSizeOptions: number[] = [15, 30, 50, 100];
 
   dataSource = new MatTableDataSource(this.exoplanets) ;
   displayedColumns: string[] = ['star','planet', 'period','distance','method', 'year', 'mass'];
@@ -23,7 +29,7 @@ export class ExoplanetsComponent implements OnInit {
   ngOnInit(): void {
     // this.getExoplanets();
     this.getExoplanetsArray();
-    this.lengthPaginator = `${this.exoplanets.length}`
+    this.length = `${this.exoplanets.length}`
   }
 
   getExoplanetsArray(){
@@ -36,6 +42,16 @@ export class ExoplanetsComponent implements OnInit {
     };
   }
 
+  ngAfterViewInit(): void {
+
+    console.log(this.paginator?.page.subscribe(data => {
+      console.log(data.pageSize);
+      console.log(data.pageIndex);
+
+    }));
+
+
+  }
   // getExoplanets(){
   //   this.apiExoplanets.getQuery().subscribe(data => {
   //     console.log(data[2])
