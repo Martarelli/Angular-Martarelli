c961651661b63a6a75e406a420c8c0aa03d4c76d
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-18 01:45:06 -0300

    pagina de contato montada

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   constructor() { }
+  contactImage: string = "../../../assets/images/contactsvg.svg"
+  linkedin: string = "../../../assets/images/linkedin.png"
+  whatsapp: string = "../../../assets/images/whatsapp.png"
+  github: string = "../../../assets/images/github.png"
 
+  linkedinProfile: string = "https://www.linkedin.com/in/rmartarelli/"
+  whatsappApi: string = "https://api.whatsapp.com/send?phone=018997971831"
+  githubProfile: string ="https://github.com/Martarelli"
   ngOnInit(): void {
   }
 
